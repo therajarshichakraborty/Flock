6faba3b26a7411f40f6555bb487276c87267c4b1
Rajarshi Chakraborty <rajarshichakraborty2005@gmail.com>
2026-09-04 01:19:13 +0530

    setup done

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,10 +1,9 @@
-import './global.css';
 import { Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-blue-500">Welcome to Nativewind!</Text>
+    <View className="flex-1 items-center justify-center bg-background">
+      <Text className="text-xl font-bold text-primary">Welcome to Nativewind!</Text>
     </View>
   );
 }
