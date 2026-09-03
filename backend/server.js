ac3b3c72ae234c765d8d7908c6b56c309d9dfc6b
Rajarshi Chakraborty <rajarshichakraborty2005@gmail.com>
2026-09-04 00:33:08 +0530

    feat: initialize backend project with Express, Socket.io, and Clerk authentication

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import http from "node:http";
+import http, {createServer} from "node:http";
 import { Server } from "socket.io";
 import { clerkMiddleware } from "@clerk/express";
 
@@ -20,10 +20,11 @@ import { connectDB } from "./config/db.js";
 import { arcjetMiddleware } from "./middleware/arcjet.middleware.js";
 
 const app = express();
-const server = http.createServer(app);
+const server = createServer(app);
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 
 const bootStrap = async () => {
   try {
